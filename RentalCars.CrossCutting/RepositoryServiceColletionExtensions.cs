d7d3759a61dc6ed7a51f593da9420c06747e075e
Igor Tessaro <igorstessaro57@gmail.com>
2022-03-31 21:30:04 -0300

    Add project files.

---
+++ RentalCars.CrossCutting/RepositoryServiceColletionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RentalCars.Domain.Repositories;
+using RentalCars.Infrastructure.Repositories.Relational;
+
+namespace RentalCars.CrossCutting
+{
+    public static class RepositoryServiceColletionExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ICarRespository, CarRespository>();
+
+            return services;
+        }
+    }
+}
---
+++ RentalCars.Infrastructure/Repositories/Relational/Configurations/CarConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RentalCars.Domain.Entities;
+
+namespace RentalCars.Infrastructure.Repositories.Relational.Configurations
+{
+    internal class CarConfiguration : IEntityTypeConfiguration<Car>
+    {
+        public void Configure(EntityTypeBuilder<Car> builder)
+        {
+            builder.ToTable("Car", "RentalCars");
+
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("Id");
+
+            builder.Property(e => e.Type).HasColumnName("Type");
+            builder.Property(e => e.ModelYear).HasColumnName("Type");
+            builder.Property(e => e.ManufactureYear).HasColumnName("Type");
+            builder.Property(e => e.Brand).HasColumnName("Type");
+            builder.Property(e => e.Color).HasColumnName("Type");
+            builder.Property(e => e.Plate).HasColumnName("Type");
+        }
+    }
+}
